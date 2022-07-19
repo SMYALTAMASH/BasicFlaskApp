b26a0a709e52779560393cf16125b0da886c2485
SMY ALTAMASH <30286162+SMYALTAMASH@users.noreply.github.com>
2022-07-19 16:53:50 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -47,4 +47,4 @@ def invalid_route(e):
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=False,host='0.0.0.0',port=9090)
